88d4e15688e4cafc069ea9212f36a350de652b3b
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2021-11-24 15:30:16 +0200

    Create Bit Strings.cpp

---
+++ CSES/Bit Strings.cpp
@@ -0,0 +1,50 @@
+// https://cses.fi/problemset/task/1617/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define nline '\n';
+#define len(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define e 2.718281828459045
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+int dx4[4] = {-1, 1, 0, 0};
+int dy4[4] = {-0, 0, -1, 1};
+const double PI = 2 * asin(1);
+const int MOD = 1e9 + 7;
+
+ll pw(ll b, ll p) {   // O(log(p) base 2)
+    if (p == 0 && b == 0) return 0;
+    if (p == 0) return 1;
+    ll sq = pw(b % MOD, (p / 2) % MOD);
+    sq = sq % MOD * sq % MOD;
+    if (p & 1) sq = sq % MOD * b % MOD;
+    return sq % MOD;
+}
+
+void IO() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+//.....................................................................................//
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%lld", pw(2, n) % MOD);
+}
+/*
+     Do not make STUPID MISTAKES!
+     NEVER to assume something not mentioned!
+     Thinking Concretely, Symbolically, Pictorically..
+     Think in the smallest boundaries (e.g. n = 0, 1, 2 - R*C = {1*1, 1*2, 2*1, 2, 2} - str = "", str = "a", ...)
+     Think in the largest boundaries (e.g. n = MAX, array is fully, string has max characters, ...)
+     Think in especial cases (array filled with zeros, 1 1 1 1, .....)
+     Make sure that you know exactly what is output and its "format"
+*/
