663e2876ea40297dcdfb4544bfd752f60b2f5ff5
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2021-12-10 17:45:03 +0200

    Create Virtual Friends.cpp

---
+++ UVA/Virtual Friends.cpp
@@ -0,0 +1,78 @@
+// https://vjudge.net/problem/UVA-11503
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define nline '\n';
+#define len(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define e 2.718281828459045
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+int dx4[4] = {-1, 1, 0, 0};
+int dy4[4] = {-0, 0, -1, 1};
+const double PI = 2 * asin(1);
+const int MOD = 1e9 + 7;
+const ll OO = 1e8;
+
+
+ll pw(ll b, ll p) {   // O(log(p) base 2)
+    if (p == 0 && b == 0) return 0;
+    if (p == 0) return 1;
+    ll sq = pw(b, p / 2);
+    sq = sq * sq;
+    if (p & 1) sq = sq * b;
+    return sq;
+}
+
+void IO() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+//.....................................................................................//
+map<string, string> parent;
+map<string, int> ranks;
+string findSet(string a){
+    if(a == parent[a]) return a;
+    return parent[a] = findSet(parent[a]);
+}
+void link(string a, string b){
+    if(ranks[a] > ranks[b]) swap(a, b);
+    parent[a] = b;
+    if(ranks[a] <= ranks[b]) ranks[b] += ranks[a];
+}
+int unionSet(string a, string b){
+    string x = findSet(a);
+    string y = findSet(b);
+    if(x != y){
+        link(x, y);
+    }
+    return max(ranks[x], ranks[y]);
+}
+int main() {
+    int t; cin >> t;
+    while(t--){
+        parent.clear();
+        ranks.clear();
+        int n; cin >> n;
+        while(n--){
+            string a, b; cin >> a >> b;
+            if(parent.count(a) == 0){
+                parent[a] = a;
+                ranks[a] = 1;
+            }
+            if(parent.count(b) == 0){
+                parent[b] = b;
+                ranks[b] = 1;
+            }
+            cout << unionSet(a, b) << nline;
+        }
+
+    }
+}
