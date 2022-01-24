77dcf0f7d607b961f7f00173f68f2439e21947cd
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2022-01-24 21:18:15 +0200

    Create To Carry or not to Carry.cpp

---
+++ UVA/To Carry or not to Carry.cpp
@@ -0,0 +1,93 @@
+// https://vjudge.net/problem/UVA-10469
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define nline '\n';
+#define len(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define e 2.718281828459045
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+int dx4[4] = {-1, 1, 0, 0};
+int dy4[4] = {-0, 0, -1, 1};
+const double PI = 2 * asin(1);
+const int MOD = 1e9 + 7;
+const ll OO = 1e18;
+const double eps = 1e-9;
+
+int dcomp(double a, double b) {
+    return fabs(a - b) <= eps ? 0 : a < b ? 1 : -1;
+}
+
+bool powerOfTwo(int n) {
+    return n && (!(n & (n - 1)));
+}
+
+void IO() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+ll pw(ll a, ll p) {
+    if (p == 0) return 1;
+    ll result = 1;
+    while (p > 0) {
+        if (p & 1) result *= a;
+        a *= a;
+        p >>= 1;
+    }
+    return result;
+}
+
+//.....................................................................................//
+string letters = "0123456789ABCDEF";
+
+int toInt(char c) { return letters.find(c); }
+
+int FromAnyBasetoDecimal(string in, int base) {
+    int res = 0;
+    for (size_t i = 0; i < in.size(); ++i)
+        res *= base, res += toInt(in[i]);
+    return res;
+}
+
+string FromDecimaltoAnyBase(int number, int base) {
+    if (number == 0)
+        return "0";
+
+    string res = "";
+    for (; number; number /= base)
+        res = letters[number % base] + res;
+    return res;
+}
+
+int main() {
+    int n, m;
+    while (scanf("%d %d", &n, &m) == 2) {
+        string a = FromDecimaltoAnyBase(n, 2);
+        string b = FromDecimaltoAnyBase(m, 2);
+        string ans = "";
+        int i = len(a) - 1, j = len(b) - 1;
+        while (i >= 0 && j >= 0) {
+            if (a[i] == b[j])
+                ans = "0" + ans;
+            else ans = "1" + ans;
+            i--, j--;
+        }
+        while (i >= 0) {
+            ans = a[i] + ans;
+            i--;
+        }
+        while (j >= 0) {
+            ans = b[j] + ans;
+            j--;
+        }
+        printf("%d\n", FromAnyBasetoDecimal(ans, 2));
+    }
+}
