d23312e266be835f1e0643bd95e67971c87dd6d3
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2022-06-22 12:00:44 +0200

    Create  d1000000.cpp

---
+++ Google/d1000000.cpp
@@ -0,0 +1,65 @@
+// https://codingcompetitions.withgoogle.com/codejam/round/0000000000876ff1/0000000000a46471
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define nline '\n';
+#define len(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define e 2.718281828459045
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+int dx4[4] = {-1, 1, 0, 0};
+int dy4[4] = {0, 0, -1, 1};
+const double PI = 2 * asin(1);
+const int MOD = 1e9 + 7;
+const ll OO = 1e18;
+const double eps = 1e-9;
+
+int dcomp(double a, double b) {
+    return fabs(a - b) <= eps ? 0 : a < b ? 1 : -1;
+}
+
+bool powerOfTwo(int n) {
+    return n && (!(n & (n - 1)));
+}
+
+void IO() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+ll pw(ll a, ll p) {
+    if (p == 0) return 1;
+    ll result = 1;
+    while (p > 0) {
+        if (p & 1) result *= a;
+        a *= a;
+        p >>= 1;
+    }
+    return result;
+}
+
+//.....................................................................................//
+int main() {
+    int t; cin >> t;
+    for (int k = 1; k <= t; ++k) {
+        int n; cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> v[i];
+        }
+        sort(all(v));
+        int ans = 0;
+        int l = 1;
+        for (int i = 0; i < n; ++i) {
+            if(v[i] >= l) ans++, l++;
+        }
+        cout << "Case #" << k << ": " << ans << nline;
+    }
+}
