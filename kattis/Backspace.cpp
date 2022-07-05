19aa6855b9f1dadeb8229e1dde0227878a816131
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2022-07-06 00:25:32 +0200

    Create Backspace.cpp

---
+++ kattis/Backspace.cpp
@@ -0,0 +1,80 @@
+// https://open.kattis.com/problems/backspace
+
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define nl '\n';
+#define len(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define e 2.718281828459045
+#define IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+int dx4[4] = {-1, 1, 0, 0};
+int dy4[4] = {0, 0, -1, 1};
+const double PI = 2 * asin(1);
+const int MOD = 1e9 + 7;
+const ll OO = 1e18;
+const double eps = 1e-9;
+
+bool isVowel(char s) {
+    s = tolower(s);
+    if (s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u') return true;
+    return false;
+}
+
+ll toInt(string s) {
+    ll r = 0;
+    istringstream sin(s);
+    sin >> r;
+    return r;
+}
+
+string toString(ll number) {
+    stringstream ss;
+    ss << number;
+    return ss.str();
+}
+
+int dcomp(double a, double b) {
+    return fabs(a - b) <= eps ? 0 : a < b ? 1 : -1;
+}
+
+bool powerOfTwo(ll n) {
+    return n && (!(n & (n - 1)));
+}
+
+
+ll binPow(ll a, ll p) {
+    ll result = 1;
+    while (p > 0) {
+        if (p & 1) result *= a;
+        a *= a;
+        p >>= 1;
+    }
+    return result;
+}
+
+//.........................................(^_^)............................................//
+
+int main() {
+    string s; cin >> s;
+    stack<char> st;
+    for (int i = 0; i < len(s); ++i) {
+        if(s[i] == '<') st.pop();
+        else st.push(s[i]);
+    }
+    string t = "";
+    while(!st.empty()){
+        t += st.top();
+        st.pop();
+    }
+    for (int i = len(t) - 1 ; i >= 0; --i) {
+        cout << t[i];
+    }
+}
+
