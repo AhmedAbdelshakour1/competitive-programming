62b0b307b9038d65a89c71e0171f5f3c9188235d
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2022-10-24 11:38:37 +0200

    Create Increasing Array.cpp

---
+++ CSES/Increasing Array.cpp
@@ -0,0 +1,97 @@
+// https://cses.fi/problemset/task/1094/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "debug.hpp"
+#else
+#define debug(...)
+#define debug_itr(...)
+#endif
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define nl '\n'
+#define len(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) (x).rbegin(),(x).rend()
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+int dx4[4] = {-1, 1, 0, 0};
+int dy4[4] = {0, 0, -1, 1};
+const double PI = 2 * asin(1);
+const int MOD = 1e9 + 7;
+const int INF = (int) 2e9 + 10;
+const ll OO = 1e18;
+const double eps = 1e-9;
+
+void IO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    #endif
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+}
+ll toInt(string s) {
+    ll r = 0;
+    istringstream sin(s);
+    sin >> r;
+    return r;
+}
+
+string toString(ll number) {
+    stringstream ss;
+    ss << number;
+    return ss.str();
+}
+
+string to_upper(string a) {
+    for (char &i : a) if (i >= 'a' && i <= 'z') i -= 'a' - 'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (char &i : a) if (i >= 'A' && i <= 'Z') i += 'a' - 'A';
+    return a;
+}
+
+int dcomp(double a, double b) { return fabs(a - b) <= eps ? 0 : a < b ? 1 : -1; }
+
+bool isPowerOfTwo(ll n) { return n && (!(n & (n - 1))); }
+
+
+ll binPow(ll a, ll p) {
+    ll result = 1;
+    while (p) {
+        if (p & 1) result *= a;
+        a *= a;
+        p >>= 1;
+    }
+    return result;
+}
+
+//.........................................\ (•◡•) /............................................//
+void solve() {
+    int n; cin >> n;
+    vector<int> v(n);
+    for(int &i : v) cin >> i;
+    ll ans = 0;
+    for(int i = 1; i < n; i++){
+        if(v[i] < v[i - 1]){
+            ans += v[i - 1] - v[i];
+            v[i] = v[i - 1];
+        }
+    }
+    cout << ans << nl;
+}
+int main() {
+    IO();
+    int t = 1;
+//    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
