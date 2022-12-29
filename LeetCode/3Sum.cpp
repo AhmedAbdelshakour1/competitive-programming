421d48460b3b9d1e8fb9125e7d51f891514d5418
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2022-12-29 22:09:00 +0200

    Create 3Sum.cpp

---
+++ LeetCode/3Sum.cpp
@@ -0,0 +1,26 @@
+https://leetcode.com/problems/3sum/
+
+class Solution {
+public:
+vector<vector<int>> threeSum(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    for(int i = 0; i < n; i++) {
+        if (i && nums[i] == nums[i - 1]) continue;
+        int l = i + 1, r = n - 1, currL = 1e9, currR = 1e9;
+        while (l < r) {
+            if(nums[i] + nums[l] + nums[r] < 0) l++;
+            else if(nums[i] + nums[l] + nums[r] > 0) r--;
+            else{
+                if(currL != nums[l] && currR != nums[r]){
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    currL = nums[l], currR = nums[r];
+                }
+                l++, r--;
+            }
+        }
+    }
+    return ans;
+}
+};
