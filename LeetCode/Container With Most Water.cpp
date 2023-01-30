89dcb4da299b767e12c071f4f8c001d40d30484f
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2023-01-30 16:32:08 +0200

    Create Container With Most Water.cpp

---
+++ LeetCode/Container With Most Water.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/container-with-most-water/description/
+
+int maxArea(vector<int>& height) {
+    int l = 0, r = height.size() - 1;
+    int ans = 0;
+    while(l < r){
+        ans = max(ans, (r - l) * min(height[l], height[r]));
+        if(height[l] <= height[r]) l++;
+        else r--;
+    }
+    return ans;
+}
