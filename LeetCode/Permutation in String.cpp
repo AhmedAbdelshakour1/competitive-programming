792c75a40f7211e17b45de0384bb44cfd340310a
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2023-02-05 16:24:08 +0200

    Update Permutation in String.cpp

--- LeetCode/Permutation in String.cpp
+++ LeetCode/Permutation in String.cpp
@@ -1,5 +1,21 @@
 // https://leetcode.com/problems/permutation-in-string/description/
 
+// Sliding Window
+bool checkInclusion(string s1, string s2) {
+    int n = s2.size(), m = s1.size();
+    if(m > n) return false;
+    vector<int> freqN(26), freqM(26);
+    for (int i = 0; i < m; ++i) freqM[s1[i] - 'a']++, freqN[s2[i] - 'a']++;
+    if(freqN == freqM) return true;
+    for (int i = m; i < n; ++i) {
+        freqN[s2[i] - 'a']++;
+        freqN[s2[i - m] - 'a']--;
+        if(freqN == freqM) return true;
+    }
+    return false;
+}
+
+// Another solution
 bool checkInclusion(string s1, string s2) {
     if(s1.size() > s2.size()) return false;
     vector<int> freq(26);
