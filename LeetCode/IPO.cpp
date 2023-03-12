93b9622054d361fef33b64a8ab6bc2b5a60be85b
AhmedAbdelshakour1 <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2023-03-12 16:06:52 +0200

    Create

---
+++ LeetCode/IPO.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/ipo/description/
+
+class Solution {
+public:
+int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+    int n = profits.size();
+    vector<pair<int, int>> v;
+    for (int i = 0; i < n; ++i)
+        v.push_back({capital[i], profits[i]});
+    sort(v.begin(), v.end());
+    priority_queue<int> pq;
+    int i = 0;
+    while(k--){
+        while(i < n && v[i].first <= w) pq.push(v[i].second), i++;
+        if(pq.empty()) break;
+        w += pq.top();
+        pq.pop();
+    }
+    return w;
+}
+};
