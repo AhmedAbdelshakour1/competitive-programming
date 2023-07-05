d3aee513882d902f691b4812d1d22c04fd85a87f
Ahmed Abdelshakour <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2023-07-05 11:06:34 +0300

    Create Concatenation of Array.cpp

---
+++ LeetCode/Concatenation of Array.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for(int i = 0; i < 2 * n; i++){
+            ans.push_back(nums[i % n]);
+        }
+        return ans;
+    }
+};
