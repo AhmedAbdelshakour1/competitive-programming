cee1b7dec4f2f3877e14d298439a711fae038ddf
Ahmed Abdelshakour <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2023-08-16 18:03:44 +0300

    Create Remove Element.cpp

---
+++ LeetCode/Remove Element.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/remove-element/description/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val)
+                swap(nums[k++], nums[i]);
+        }
+        return k;
+    }
+};
