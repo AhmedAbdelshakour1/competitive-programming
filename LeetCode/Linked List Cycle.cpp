eb379c26810f3ea002715e2e495a96c85ea71e00
Ahmed Abdelshakour <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2023-09-05 01:33:50 +0300

    Update Linked List Cycle.cpp

--- LeetCode/Linked List Cycle.cpp
+++ LeetCode/Linked List Cycle.cpp
@@ -21,3 +21,18 @@ public:
         return false;
     }
 };
+
+// Another Solution using Floyd’s slow and fast pointers approach
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast && fast -> next){
+            fast = fast -> next -> next;
+            slow = slow -> next;
+            if(fast == slow) return true;
+        }
+        return false;
+    }
+};
