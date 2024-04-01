7ce85de3ee7fe38855c2940fdcfa65736165de88
Ahmed Abdelshakour <91353415+AhmedAbdelshakour1@users.noreply.github.com>
2024-04-01 22:09:30 +0200

    Create Valid Palindrome.cpp

---
+++ LeetCode/Valid Palindrome.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/valid-palindrome/description/
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while(l < r){
+            if(!isalnum(s[l])){
+                l++;
+                continue;
+            }
+            if(!isalnum(s[r])) {
+                r--;
+                continue;
+            }
+            if(tolower(s[l]) != tolower(s[r])) return false;
+            l++, r--;
+        }
+        return true;
+    }
+};
